bf58b6dc3d11c2d56d996e772e76583ef0a96974
Venkat Nagappan <wenkatn@gmail.com>
2020-09-25 22:38:47 +0100

    powerlevel10k config added

--- compute_instance/scripts/bootstrap.sh
+++ compute_instance/scripts/bootstrap.sh
@@ -4,6 +4,7 @@ apt update
 apt install -y zsh zsh-autosuggestions zsh-syntax-highlighting
 chsh -s /usr/bin/zsh venkatn
 sudo -u venkatn bash -c "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~venkatn/.powerlevel10k"
+sudo -u venkatn bash -c "wget https://raw.githubusercontent.com/justmeandopensource/gcp/master/compute_instance/scripts/.p10k.zsh -O ~venkatn/.p10k.zsh"
 
 cat >> ~venkatn/.zshrc <<EOF
 source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
